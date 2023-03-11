87448f250a9a4baf1acb921d1d69ce80552d8591
ShiminZhang <ShiminZhang@github.com>
2023-03-11 22:50:21 +0800

    update

--- manage.py
+++ manage.py
@@ -16,6 +16,18 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+def StartRobot():
+    import werobot
+    robot = werobot.WeRoBot(token='tokenhere')
+
+    @robot.handler
+    def hello(message):
+        return 'Hello World!'
+
+    # 让服务器监听在 0.0.0.0:80
+    robot.config['HOST'] = '0.0.0.0'
+    robot.config['PORT'] = 80
+    robot.run()
 
 if __name__ == '__main__':
     main()
